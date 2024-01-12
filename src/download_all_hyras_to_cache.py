91951bd22657fa74184b4b468dd9a73bf3f0c953
Mirko Mälicke <mirko@hydrocode.de>
2024-01-12 11:36:29 +0100

    use many threads for hyras-PR

--- src/download_all_hyras_to_cache.py
+++ src/download_all_hyras_to_cache.py
@@ -14,13 +14,13 @@ from db import DuckProcessor
 load_dotenv()
 
 
-def download_one(variable: str):
+def download_one(variable: str, start: int = 1951, end: int = 2024):
     processor = DuckProcessor(
         local_hyras_cache='./hyras_cache'
     )
     
     # go for each year
-    for year in tqdm(range(1950, 2024)):
+    for year in tqdm(range(start, end)):
         try:
             processor.download_hyras(variable=variable, year=year)
         except Exception as e:
@@ -30,12 +30,15 @@ def download_one(variable: str):
 
 
 def download():
-    variables = ('hurs', 'rsds', 'pr', 'tas', 'tasmax', 'tasmin')
+    # 'pr takes way longer than the others, so we start it many times
+    variables = ('hurs', 'rsds', 'tas', 'tasmax', 'tasmin', 'pr', 'pr', 'pr', 'pr', 'pr', 'pr', 'pr')
+    starts = (1951, 1951, 1951, 1951, 1951, 1951, 1961, 1971, 1981, 1991, 2001, 2011)
+    ends =   (2024, 2024, 2024, 2024, 2024, 1961, 1971, 1981, 1991, 2001, 2011, 2024)
 
     # go multithreading for faster download
     with ThreadPoolExecutor() as executor:
         try:
-            list(executor.map(download_one, variables, timeout=60*50))
+            list(executor.map(download_one, variables, starts, ends, timeout=60*50))
         except TimeoutError:
             print("The full hyras dataset could not be downloaded within 50 Minutes")
 
